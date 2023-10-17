c1e053bf503b2c98cc1f76f5a867df25a726a35b
annanodiyo <annanodiyo@gmail.com>
2023-10-17 06:44:35 +0300

    worked on adding tasks to the todoList

--- main.js
+++ main.js
@@ -1,90 +1,97 @@
 const todos = [
-    { id: 1, text: 'eat pizza', done: false},
-    { id: 2, text: 'eat mango', done: false},
-    { id: 3, text: 'eat pineapple', done: false}
+  { id: 1, text: 'eat pizza', done: false},
+  { id: 2, text: 'eat mango', done: false},
+  { id: 3, text: 'eat pineapple', done: false}
 ];
-const todoForm=document.querySelector('.mytask');
+const todoForm=document.querySelector('.mytasks');
 const todoInput = document.getElementById('todoInput');
 const todoList = document.getElementById('todoList');
 const statusText = document.getElementById('statusText');
+const addTaskbtn = document.getElementById('submit');
+addTaskbtn.addEventListener('click',addTodo);
+todoForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  addTodo();
+});
+
 function renderTodos() {
-    todoList.innerHTML = "";
-    todos.forEach((todo => {
-        const li = document.createElement('li');
-        li.classList.add('todo-item');
-        if(todo.done){
-            li.classList.add("completed");
-        }
-        const tasknext = document.createElement("input");
-        tasknext.type="checkbox";
-        tasknext.next = todo.done;
-        tasknext.addEventListener("change", () => toggleDone(todo.id));
+  todoList.innerHTML = "";
+  todos.forEach((todo => {
+      const li = document.createElement('li');
+      li.classList.add('todo-item');
+      if(todo.done){
+          li.classList.add("completed");
+      }
+      const tasknext = document.createElement("input");
+      tasknext.type="checkbox";
+      tasknext.next = todo.done;
+      tasknext.addEventListener("change", () => toggleDone(todo.id));
 
-        li.appendChild(tasknext);
-        const span=document.createElement('span');
-        span.textContent = todo.text;
-        li.appendChild(span);
+      li.appendChild(tasknext);
+      const span=document.createElement('span');
+      span.textContent = todo.text;
+      li.appendChild(span);
 
-        li.innerHTML = todo.text;
-        todoList.appendChild(li);
-    }));
-    showStatus();
+      li.innerHTML = todo.text;
+      todoList.appendChild(li);
+  }));
+  showStatus();
 }
 renderTodos();
 function showStatus() {
-  if (!todos.length) {
-    statusText.textContent = "You have no tasks. 😊😊";
-    return;
-  }
+if (!todos.length) {
+  statusText.textContent = "You have no tasks. 😊😊";
+  return;
+}
 
-  const pending = todos.filter((todo) => !todo.done).length;
-  if (pending === 0) {
-    statusText.textContent = "All done! 🎉";
-  } else if (pending === 1) {
-    statusText.textContent = "1 task remaining";
-  } else {
-    statusText.textContent = `${pending} / ${todos.length} tasks remaining 😬`;
-  }
+const pending = todos.filter((todo) => !todo.done).length;
+if (pending === 0) {
+  statusText.textContent = "All done! 🎉";
+} else if (pending === 1) {
+  statusText.textContent = "1 task remaining";
+} else {
+  statusText.textContent = `${pending} / ${todos.length} tasks remaining 😬`;
+}
 }
 
 function addTodo() {
-  const newTodoText = todoInput.value.trim();
-  if (!newTodoText) {
-    alert("Please enter a valid todo item");
-    // return;
-  }
+const newTodoText = todoInput.value.trim();
+if (!newTodoText) {
+  alert("Please enter a valid todo item");
+  return;
+}
 
-  const newTodo = {
-    id: todos.length + 1,
-    text: newTodoText,
-    done: false,
-  };
+const newTodo = {
+  id: todos.length + 1,
+  text: newTodoText,
+  done: false,
+};
 
-  todos.unshift(newTodo);
+todos.unshift(newTodo);
 
-  saveTodos();
-  renderTodos();
+saveTodos();
+renderTodos();
 
-  todoInput.value = "";
+todoInput.value = "";
 }
 
 function toggleDone(id) {
-  const todo = todos.find((todo) => todo.id === id);
-  todo.done = !todo.done;
+const todo = todos.find((todo) => todo.id === id);
+todo.done = !todo.done;
 
-  saveTodos();
-  renderTodos();
+saveTodos();
+renderTodos();
 }
 
 function removeTodo(id) {
-  const todoIndex = todos.findIndex((todo) => todo.id === id);
-  todos.splice(todoIndex, 1);
+const todoIndex = todos.findIndex((todo) => todo.id === id);
+todos.splice(todoIndex, 1);
 
-  saveTodos();
-  renderTodos();
+saveTodos();
+renderTodos();
 }
 
 
-// function saveTodos() {
-  // localStorage.setItem("todos", JSON.stringify(todos));
-// }
+function saveTodos() {
+localStorage.setItem("todos", JSON.stringify(todos));
+}
